0eadf7bc9ecaa7848660af6d12e3092d5eabd22e
Jaremy Hatler <hatler.jaremy@gmail.com>
2024-06-25 09:54:18 +0000

    feat: add packer to build runner AMI
    This adds a packer/ directory to the repository that will house the
    files related to building machine images using Packer. The first
    machine image that has been added is a starting point for a GitHub
    Actions runner. This image is based on Ubuntu 22.04 and doesn't receive
    any configuration at this time. That will be added in subsequent
    commits.
    
    This commit also adds a GitHub Actions workflow that will build the
    runner AMI using Packer. This workflow is triggered by updates to the
    packer/ directory (PR and push events). It also will run every 2 weeks
    to ensure that the runner AMI is kept up to date.
    
    Refs: #273
    Signed-off-by: Jaremy Hatler <hatler.jaremy@gmail.com>

---
+++ packer/runner/scripts/clean.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex -o pipefail
+
+cloud-init clean --logs --machine-id --configs all
+rm -f /etc/sudoers.d/90-cloud-init-users
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+find /var/cache/apt -type f -print0 | xargs -0 rm -f
+touch -d '1970-01-01 0:00:00' /var/lib/apt/lists
+
+# Stop the journal, clear machine-specific logs, and truncate the rest
+systemctl stop systemd-journald.service systemd-journald{,-dev-log,-audit}.socket
+rm -rf /var/log/journal/* /var/log/amazon/ssm/audits/* /var/log/apt/*
+find /var/log -type f -print0 | xargs -0 truncate --size=0
+
+# Clear the tmp directories
+find /var/tmp /tmp -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} \;
+
+# Recommended by systemd for imaging
+rm -f /var/lib/systemd/random-seed /etc/hostname
+
+# Prevent known host or key issues
+rm -f /etc/ssh/ssh_host_* /root/.ssh/authorized_keys
+
+# Delete the setup files
+rm -rf /setup
+
+# Deletes the ubuntu group too
+# No sudo after this, including in packer provisioners
+userdel -rf ubuntu
