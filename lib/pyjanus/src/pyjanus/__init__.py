bbdb36e4a217b4e2beca13c1909a494200d4213a
Jaremy Hatler <hatler.jaremy@gmail.com>
2024-07-04 16:58:34 -0400

    feat: bootstrap pyJanus testing (#400)
    This commit adds the necessary files to bootstrap testing for the
    pyjanus library. The tests don't validate any of the library's code
    yet, but instead some basic arithmatic functions. The tests are written
    using the `pytest` framework.
    
    Refs: #172
    Signed-off-by: Jaremy Hatler <hatler.jaremy@gmail.com>

---
+++ lib/pyjanus/src/pyjanus/__init__.py
@@ -0,0 +1,15 @@
+"""pyJanus"""
+
+
+def divide(a: int, b: int) -> float:
+    """Divide two numbers."""
+    return a / b
+
+
+def add(a: int, b: int) -> int:
+    """Add two numbers together."""
+    return a + b
+
+
+def main():
+    """pyJanus main function."""
