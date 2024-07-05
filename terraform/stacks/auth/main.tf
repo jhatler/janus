35e5d4b5f2e61d96f4f753cd02effcf857bc72ac
Jaremy Hatler <hatler.jaremy@gmail.com>
2024-07-04 21:41:26 -0400

    feat: authorize control repo for AWS (#408)
    This adds the github-oidc module to the `auth` stack to authorize the
    control repo for basic AWS access.
    
    Fixes: #407
    Signed-off-by: Jaremy Hatler <hatler.jaremy@gmail.com>

--- terraform/stacks/auth/main.tf
+++ terraform/stacks/auth/main.tf
@@ -1,14 +1,15 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
-# module "github-oidc" {
-#   source  = "spacelift.io/jhatler/github-oidc/aws"
-#   version = "0.1.0"
+module "github-oidc" {
+  #checkov:skip=CKV_TF_1: Spacelift modules should be retrieved from Spacelift Module Registry
+  source  = "spacelift.io/jhatler/github-oidc/aws"
+  version = "0.2.2"
 
-#   # Required inputs
-#   github_owner      = var.control_owner
-#   github_repository = var.control_repository
-#   role_policies = [
-#     aws_iam_policy.gh_oidc_runners.arn
-#   ]
-# }
+  # Required inputs
+  github_owner      = var.control_owner
+  github_repository = var.control_repository
+  role_policies = [
+    aws_iam_policy.gh_oidc_runners.arn
+  ]
+}
